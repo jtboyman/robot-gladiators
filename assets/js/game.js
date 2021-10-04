210be2b9e7234652d5ee0697aca1ef836dad915c
Edelgard <jtboyman@gmail.com>
2021-10-04 19:06:03 -0400

    first fight lines

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,23 @@
 const playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
 
-console.log(playerName);
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
 
-function fight () {
-    window.alert("The fight has begun!!");
-}
+let fight = function() {
+    //alert players that they are starting the round
+    window.alert("Welcome to ROBOT GLADIATORS!");
 
-// fight();
+    //subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the 'enemyHealth' variable.
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log( playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
+};
+
+fight();
