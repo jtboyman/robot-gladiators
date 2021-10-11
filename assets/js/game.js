1ceade0d8e60c2928a701fce34300299e55fc184
Edelgard <jtboyman@gmail.com>
2021-10-11 17:46:34 -0400

    allow shop to choose item with integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -128,23 +128,24 @@ let endGame = function() {
 let shop = function() {
     //ask player wat they want to do
     let shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    //change string to integer
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
     //using switch to carry out action
+
     switch (shopOptionPrompt) {
-        case "refill":
-        case "REFILL":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "upgrade":
-        case "UPGRADE":
+        case 2:
             playerInfo.upgradeAttack();
             break;
             
-        case "leave":
-        case "LEAVE":
+        case 3:
             window.alert("Leaving the store.");
 
             //do noting, so function will end
